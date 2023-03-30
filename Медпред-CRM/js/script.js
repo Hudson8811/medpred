c5bfa6e89d5f1f58e042d8c6b8212b1e67446f4f
PipZip <37911683+PipZip@users.noreply.github.com>
2023-03-30 19:52:05 +0600

    Update script.js

--- Медпред-CRM/js/script.js
+++ Медпред-CRM/js/script.js
@@ -1,65 +1,65 @@
 $(document).ready(function () {
-    const da = new DynamicAdapt("max");
-    da.init();
-    Fancybox.bind("[data-fancybox]");
-    AOS.init({
+	const da = new DynamicAdapt("max");
+	da.init();
+	Fancybox.bind("[data-fancybox]");
+	AOS.init({
 
-        offset: 200,
-        once: true
+		offset: 200,
+		once: true
 
-    });
-    $('input[type="tel"]').inputmask({ "mask": "+7(999) 999-9999" });
+	});
+	$('input[type="tel"]').inputmask({ "mask": "+7(999) 999-9999" });
 
-    $(".detail-text ol").each(function (index, value) {
-        console.log(value);
+	$(".detail-text ol").each(function (index, value) {
+		console.log(value);
 
-        let items = $(value).find('> li')
-        items.each(function (index, value) {
-            $(value).attr('data-count', index + 1)
-        });
-    });
-    $("form").submit(function (event) {
-        let form = $(this);
-        event.preventDefault();
-        form.find('button').addClass('sending');
-        form.find('input[name="page"]').val(window.location.href);
-        $.ajax({
-            url: "/mail.php",
-            data: form.serialize(),
-            type: "POST",
-            // dataType: "json"
-            error: function () { // Данные не отправлены
-                alert('Произошла ошибка, попробуйте еще раз');
-                form.find('button').removeClass('sending');
-                Fancybox.close();
-            }
-        }).done(function (data) {
-            form.find('button').removeClass('sending');
-            Fancybox.close();
-            form[0].reset();
-            Fancybox.show([
-                {
-                    src: '#form-success',
-                    type: 'inline',
-                },
-            ]);
-        })
+		let items = $(value).find('> li')
+		items.each(function (index, value) {
+			$(value).attr('data-count', index + 1)
+		});
+	});
+	$("form").submit(function (event) {
+		let form = $(this);
+		event.preventDefault();
+		form.find('button').addClass('sending');
+		form.find('input[name="page"]').val(window.location.href);
+		$.ajax({
+			url: "/mail.php",
+			data: form.serialize(),
+			type: "POST",
+			// dataType: "json"
+			error: function () { // Данные не отправлены
+				alert('Произошла ошибка, попробуйте еще раз');
+				form.find('button').removeClass('sending');
+				Fancybox.close();
+			}
+		}).done(function (data) {
+			form.find('button').removeClass('sending');
+			Fancybox.close();
+			form[0].reset();
+			Fancybox.show([
+				{
+					src: '#form-success',
+					type: 'inline',
+				},
+			]);
+		})
 
-    });
-    let str = 'Лапароскопическая'
+	});
+	let str = 'Лапароскопическая'
 })
 
 
 $(document).on('click', '.js-menu__btn', function () {
-    if (!$(this).hasClass('open')) {
-        $(this).addClass('open');
-        $('.js-menu').addClass('open');
-        $('body').addClass('lock');
-    } else {
-        $(this).removeClass('open');
-        $('.js-menu').removeClass('open');
-        $('body').removeClass('lock');
-    }
+	if (!$(this).hasClass('open')) {
+		$(this).addClass('open');
+		$('.js-menu').addClass('open');
+		$('body').addClass('lock');
+	} else {
+		$(this).removeClass('open');
+		$('.js-menu').removeClass('open');
+		$('body').removeClass('lock');
+	}
 });
 
 
@@ -74,13 +74,20 @@ $(document).ready(function () {
                 let domain = window.location.hostname;
                 let currentUrlWithProtocol = protocol + '//' + domain;
                 link = currentUrlWithProtocol + link;
-            }
 
-            let url = new URL(link);
-            let path = url.pathname;
+                let url = new URL(link);
+                let path = url.pathname;
 
-            if (currentLocation == path){
-                event.preventDefault();
+                if (currentLocation == path){
+                    event.preventDefault();
+                    let hash = this.hash;
+                    $('html, body').animate({
+                        scrollTop: $(hash).offset().top
+                    }, 800, function(){
+                        window.location.hash = hash;
+                    });
+                }
+            } else if (link.startsWith("#")){
                 let hash = this.hash;
                 $('html, body').animate({
                     scrollTop: $(hash).offset().top
